7e2d559c598e5610d8d7bd2eea399bb166f96a2d
Kiran Varaganti <Kiran.Varaganti@amd.com>
2020-02-07 12:23:29 +0530

    modified sourceme.sh to link to Blis

--- step4/sourceme.sh
+++ step4/sourceme.sh
@@ -15,12 +15,12 @@ export COMPILER_OPT_LEVEL=O3
 echo "COMPILER_OPT_LEVEL = $COMPILER_OPT_LEVEL"
 
 # Manually set the BLAS path if BLIS_USE_BLAS=true and using GNU compiler.
-#export BLAS_DIR=/u/jianyu/lib/blis
-export BLAS_DIR=/u/jianyu/lib/openblas
+export BLAS_DIR=/home/vkiran/mylibs/blismt
+#export BLAS_DIR=/u/jianyu/lib/openblas
 echo "BLAS_DIR = $BLAS_DIR"
 
 # Parallel Options
 export KMP_AFFINITY=compact,verbose
 export OMP_NUM_THREADS=1
-export BLISLAB_IC_NT=1
+#export BLISLAB_IC_NT=1
 
